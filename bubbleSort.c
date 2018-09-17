323ae53aea86a4e1181fd627edc71294dc7f73ba
Gustavo Scheffer <gustavo.scheffer@outlook.com>
2018-09-16 22:16:25 -0300

    BubbleSort was completed in 20180918

--- bubbleSort.c
+++ bubbleSort.c
@@ -6,16 +6,24 @@ int main(int argc, char const *argv[])
 	int LENGTH = 7; //constant 
 	int aux; // help the changes
 	int loop = 1; // loop interruptor
-	int arr[7] = {64,40,2,53,4,1,66}; // array
 	int cont = 0; // cont
-	int last_cont = 0; // last cont
+	int last_mod = 0; // last cont
 
 	
-	/* while used to start "for" */
+	/* Array */
+	int arr[7] = {64,40,100,33,4,2,6}; 
+	
+	/* This variable is responsible to shorten the array from the last to the first. */
+	last_mod = LENGTH;
+
+	/* While used to start "for" */
 	while (loop == 1)
 	{	
-		/*loop in array*/
-		for (int i = 0; i < LENGTH; i++)
+		/* Set loop to FALSE because if none change occur inside array. The loop will stopped. */
+		loop = 0;
+
+		/* loop in array */
+		for (int i = 0; i < last_mod; i++)
 		{	
 
 			/* change position between arr[i] and arr[i+1] */
@@ -24,19 +32,13 @@ int main(int argc, char const *argv[])
 				aux = arr[i];
 				arr[i] = arr[i+1];
 				arr[i+1] = aux;
-				cont = last_cont +1;
+				
+				// if position is changed, variable loop will be TRUE
+				loop = 1;
 			}				
 		}
-		
-		/* check if last "for" and current "for" had changes */
-		if (last_cont == cont)
-		{
-			/* turn "loop" off */	
-			loop = 0;
-		}
-
-		/* "last_cont" receive "cont" value */
-		last_cont = cont;
+		// new length to search
+		last_mod  = last_mod --;
 	}
 
 	/* "for" used to show sorted array */
