fc9bdffaf5aad7aca5e80c3fcc9ae73e3aef7916
Gustavo Scheffer <gustavo.scheffer@outlook.com>
2018-09-21 16:23:44 -0300

    new sorts prepared...

---
+++ mergeSort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+
+	/* "for" used to show sorted array */
+	for (int i = 0; i < LENGTH ; i++)
+	{
+		printf("array[%d] => %d \n",i, arr[i]);
+	}
+
+	printf("\n *** MERGE SORT ***\n");
+	printf("\n");
+	/* exit */
+	return 0;
+}
